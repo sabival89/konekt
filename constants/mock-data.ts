840cd4f77353a670ed87fdff10a90405305d9da2
Valentine Aduaka <sabival89@gmail.com>
2025-07-30 10:29:06 -0500

    Update readme file

--- constants/mock-data.ts
+++ constants/mock-data.ts
@@ -3,42 +3,42 @@ export const STORIES = [
     id: '1',
     username: 'You',
     avatar:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1556817411-31ae72fa3ea0?w=400&h=400&fit=crop',
     hasStory: false,
   },
   {
     id: '2',
-    username: 'sarah.k',
+    username: 'anderson.p',
     avatar:
-      'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=400&h=400&fit=crop',
     hasStory: true,
   },
   {
     id: '3',
     username: 'alex.m',
     avatar:
-      'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1483721310020-03333e577078?w=400&h=400&fit=crop',
     hasStory: true,
   },
   {
     id: '4',
     username: 'julia.r',
     avatar:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1556817411-31ae72fa3ea0?w=400&h=400&fit=crop',
     hasStory: true,
   },
   {
     id: '5',
     username: 'mike.rs',
     avatar:
-      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?w=400&h=400&fit=cro',
     hasStory: true,
   },
   {
     id: '6',
     username: 'jane.d',
     avatar:
-      'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1496412705862-e0088f16f791?w=400&h=400&fit=crop',
     hasStory: true,
   },
   {
@@ -52,7 +52,7 @@ export const STORIES = [
     id: '8',
     username: 'mike.rs',
     avatar:
-      'https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=400&h=400&fit=crop',
+      'https://images.unsplash.com/photo-1494599948593-3dafe8338d71?w=400&h=400&fit=crop',
     hasStory: true,
   },
 ]
